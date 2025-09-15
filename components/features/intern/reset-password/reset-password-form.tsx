e82144c7d5716fa2b1f39e102be2066a5462b76d
Ayobami Tunwase <tunwaseayobami98@gmail.com>
2025-09-15 06:11:38 +0100

    fixed vercel deployment error

--- components/features/intern/reset-password/reset-password-form.tsx
+++ components/features/intern/reset-password/reset-password-form.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -70,114 +70,116 @@ export const ResetPasswordForm = () => {
   }
 
   return (
-    <Form {...form}>
-      <form
-        method="POST"
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8"
-      >
-        <FormField
-          control={form.control}
-          name="newPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel
-                htmlFor="new-password"
-                className="text-muted-foreground font-medium"
-              >
-                New Password
-              </FormLabel>
-              <FormControl>
-                <div className="relative">
-                  <Input
-                    type={showNewPassword ? "text" : "password"}
-                    autoComplete="new-password"
-                    id="new-password"
-                    className="pr-12"
-                    {...field}
-                  />
-                  <Button
-                    type="button"
-                    id="toggle-password"
-                    aria-label="Show password as plain text. Warning: this will display your password on the screen."
-                    variant="ghost"
-                    size="icon"
-                    onClick={() =>
-                      setShowNewPassword((prevState) => !prevState)
-                    }
-                    disabled={form.getValues("newPassword").length === 0}
-                    className="absolute right-2 top-1/2 -translate-y-1/2 disabled:cursor-not-allowed"
-                  >
-                    {showNewPassword ? (
-                      <EyeOffIcon aria-hidden="true" className="size-4" />
-                    ) : (
-                      <EyeIcon aria-hidden="true" className="size-4" />
-                    )}
-                    <span className="sr-only">
-                      {showNewPassword ? "Hide" : "Show"} password
-                    </span>
-                  </Button>
-                </div>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel
-                htmlFor="confirm-new-password"
-                className="text-muted-foreground font-medium"
-              >
-                Confirm Password
-              </FormLabel>
-              <FormControl>
-                <div className="relative">
-                  <Input
-                    type={showConfirmPassword ? "text" : "password"}
-                    autoComplete="new-password"
-                    id="confirm-new-password"
-                    className="pr-12"
-                    {...field}
-                  />
-                  <Button
-                    type="button"
-                    id="toggle-confirm-password"
-                    aria-label="Show password as plain text. Warning: this will display your password on the screen."
-                    variant="ghost"
-                    size="icon"
-                    onClick={() =>
-                      setShowConfirmPassword((prevState) => !prevState)
-                    }
-                    disabled={form.getValues("confirmPassword").length === 0}
-                    className="absolute right-2 top-1/2 -translate-y-1/2 disabled:cursor-not-allowed"
-                  >
-                    {showConfirmPassword ? (
-                      <EyeOffIcon aria-hidden="true" className="size-4" />
-                    ) : (
-                      <EyeIcon aria-hidden="true" className="size-4" />
-                    )}
-                    <span className="sr-only">
-                      {showConfirmPassword ? "Hide" : "Show"} password
-                    </span>
-                  </Button>
-                </div>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button
-          type="submit"
-          disabled={mutation.isPending}
-          className="w-full rounded-3xl text-foreground cursor-pointer font-medium"
+    <Suspense fallback={<p>Loading...</p>}>
+      <Form {...form}>
+        <form
+          method="POST"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
         >
-          Reset Password
-        </Button>
-      </form>
-    </Form>
+          <FormField
+            control={form.control}
+            name="newPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel
+                  htmlFor="new-password"
+                  className="text-muted-foreground font-medium"
+                >
+                  New Password
+                </FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input
+                      type={showNewPassword ? "text" : "password"}
+                      autoComplete="new-password"
+                      id="new-password"
+                      className="pr-12"
+                      {...field}
+                    />
+                    <Button
+                      type="button"
+                      id="toggle-password"
+                      aria-label="Show password as plain text. Warning: this will display your password on the screen."
+                      variant="ghost"
+                      size="icon"
+                      onClick={() =>
+                        setShowNewPassword((prevState) => !prevState)
+                      }
+                      disabled={form.getValues("newPassword").length === 0}
+                      className="absolute right-2 top-1/2 -translate-y-1/2 disabled:cursor-not-allowed"
+                    >
+                      {showNewPassword ? (
+                        <EyeOffIcon aria-hidden="true" className="size-4" />
+                      ) : (
+                        <EyeIcon aria-hidden="true" className="size-4" />
+                      )}
+                      <span className="sr-only">
+                        {showNewPassword ? "Hide" : "Show"} password
+                      </span>
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel
+                  htmlFor="confirm-new-password"
+                  className="text-muted-foreground font-medium"
+                >
+                  Confirm Password
+                </FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input
+                      type={showConfirmPassword ? "text" : "password"}
+                      autoComplete="new-password"
+                      id="confirm-new-password"
+                      className="pr-12"
+                      {...field}
+                    />
+                    <Button
+                      type="button"
+                      id="toggle-confirm-password"
+                      aria-label="Show password as plain text. Warning: this will display your password on the screen."
+                      variant="ghost"
+                      size="icon"
+                      onClick={() =>
+                        setShowConfirmPassword((prevState) => !prevState)
+                      }
+                      disabled={form.getValues("confirmPassword").length === 0}
+                      className="absolute right-2 top-1/2 -translate-y-1/2 disabled:cursor-not-allowed"
+                    >
+                      {showConfirmPassword ? (
+                        <EyeOffIcon aria-hidden="true" className="size-4" />
+                      ) : (
+                        <EyeIcon aria-hidden="true" className="size-4" />
+                      )}
+                      <span className="sr-only">
+                        {showConfirmPassword ? "Hide" : "Show"} password
+                      </span>
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            disabled={mutation.isPending}
+            className="w-full rounded-3xl text-foreground cursor-pointer font-medium"
+          >
+            Reset Password
+          </Button>
+        </form>
+      </Form>
+    </Suspense>
   );
 };
