b43fd86b5a641e87d3230d34b89a10ca1aa26b4e
Ayobami Tunwase <tunwaseayobami98@gmail.com>
2025-09-15 06:15:11 +0100

    fixed vercel deployment error

--- app/intern/reset-password/page.tsx
+++ app/intern/reset-password/page.tsx
@@ -2,6 +2,8 @@ import { ResetPasswordForm } from "@/components/features/intern/reset-password/r
 
 import Image from "next/image";
 
+import { Suspense } from "react";
+
 export default function ResetPasswordPage() {
   return (
     <main>
@@ -15,12 +17,16 @@ export default function ResetPasswordPage() {
               height={116}
               priority
             />
-            <h2 className="font-medium text-xl leading-7 mb-1">Create new password</h2>
+            <h2 className="font-medium text-xl leading-7 mb-1">
+              Create new password
+            </h2>
             <p className="text-sm leading-5 text-muted-foreground">
               Enter your new password below.
             </p>
           </header>
-          <ResetPasswordForm />
+          <Suspense fallback={<p>Loading...</p>}>
+            <ResetPasswordForm />
+          </Suspense>
         </div>
       </section>
     </main>
